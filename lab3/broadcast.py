2b11d8c35707ef5bf99de2ad25807a22ac5e2f3f
Saad <58188800+saadam1n@users.noreply.github.com>
2025-11-08 21:20:03 -0500

    Task 1 & 2 complete

--- lab3/broadcast.py
+++ lab3/broadcast.py
@@ -8,19 +8,56 @@ def check_broadcast(x, a, b, c, d):
 
 def broadcast1(x, a, b, c, d):
     # For example, use a.copy_(x) to copy the data from x to a
-    pass
+    a.copy_(x)
+    b.copy_(x)
+    c.copy_(x)
+    d.copy_(x)
 
 
 def broadcast2(x, a, b, c, d):
-    pass
+    a.copy_(x)
+    b.copy_(a)
+    c.copy_(a)
+    d.copy_(a)
+
 
 
 def broadcast3(x, a, b, c, d):
-    pass
+    sa = torch.cuda.Stream(device='cuda:0')
+    sb = torch.cuda.Stream(device='cuda:1')
+    sc = torch.cuda.Stream(device='cuda:2')
+    sd = torch.cuda.Stream(device='cuda:3')
+    
+    num_chunks = 32
+    chunk_size = (x.numel() + num_chunks - 1) // num_chunks
+    
+    for i in range(num_chunks):
+        start = i * chunk_size
+        end = min((i + 1) * chunk_size, x.numel())
+        
+        with torch.cuda.stream(sa):
+            a.view(-1)[start:end].copy_(x.view(-1)[start:end], non_blocking=True)
+        
+        with torch.cuda.stream(sb):
+            sb.wait_stream(sa)
+            b.view(-1)[start:end].copy_(a.view(-1)[start:end], non_blocking=True)
+        
+        with torch.cuda.stream(sc):
+            sc.wait_stream(sa)
+            c.view(-1)[start:end].copy_(a.view(-1)[start:end], non_blocking=True)
+        
+        with torch.cuda.stream(sd):
+            sd.wait_stream(sa)
+            d.view(-1)[start:end].copy_(a.view(-1)[start:end], non_blocking=True)
+    
+    sa.synchronize()
+    sb.synchronize()
+    sc.synchronize()
+    sd.synchronize()
 
-def timeit(boradcast):
+def timeit(boradcast, pin_memory=False):
 
-    x = torch.randn((100000, 10000))
+    x = torch.randn((100000, 10000), pin_memory=pin_memory)
     a = torch.zeros((100000, 10000), device="cuda:0")
     b = torch.zeros((100000, 10000), device="cuda:1")
     c = torch.zeros((100000, 10000), device="cuda:2")
@@ -46,7 +83,17 @@ def timeit(boradcast):
     assert check_broadcast(x, a, b, c, d)
     return toc - tic
 
+import tqdm
+def avg_timeit(*args):
+    num_iterations = 16
 
-print('Running time for broadcast1:', timeit(broadcast1), '(s)')
-print('Running time for broadcast2:', timeit(broadcast2), '(s)')
-print('Running time for broadcast3:', timeit(broadcast3), '(s)')
+    timing_data = []
+    for _ in tqdm.tqdm(range(num_iterations)):
+        timing_data.append(timeit(*args))
+
+    return sum(timing_data) / num_iterations
+
+
+print('Running time for broadcast1:', avg_timeit(broadcast1), '(s)')
+print('Running time for broadcast2:', avg_timeit(broadcast2), '(s)')
+print('Running time for broadcast3:', avg_timeit(broadcast3, True), '(s)')
--- lab3/report.md
+++ lab3/report.md
@@ -1,1 +1,31 @@
 # Lab 3
+
+Saad Amin
+
+## Task 1
+
+![Task 1 image](task_1.png)
+
+It appears PACE gave me 8 GPUs even though I asked only for 4. 
+
+What this means is that:
+- GPUs are connected using NVLink, which means they should have (relatively) fast memory access between each other.
+- We have 2 NUMA nodes, and GPUs0-3 are wired via PCIe to node 0 and GPUs4-7 are wired to node 1
+
+## Task 2
+
+```
+Running time for broadcast1: 1.3572168946266174 (s)
+Running time for broadcast2: 0.3723844289779663 (s)
+Running time for broadcast3: 0.07500745356082916 (s)
+```
+
+Modifications made to code:
+- Timing data is the average of 16 runs to get more accurate data
+
+Design choices:
+- Host tensor `a` is allocated using `pin_memory=True` instead. 
+- All transfers use `non_blocking=True`
+- All transfers happen on their own streams
+- Transfers from `x` -> `a` -> other GPUs have been pipelined 
+    - We transfer contigous blocks from `x` to `a`, wait for it to finish, and then start the transfer of that block to other GPUs while we transfer the next block to `a`
